dc4296b65b06b1a5a585922ab693c54754cc2d28
diegolikescode <diegoprestes.git@hotmail.com>
2026-01-07 20:44:02 -0300

    just forget about it

--- frontend/src/protocol/message.ts
+++
@@ -1,7 +0,0 @@
-import { Buffer } from 'buffer'
-
-export function buildHandshake() {
-  // const buf = Buffer.alloc(68)
-  // buf.
-}
-
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function main() {
+    console.log('hey hey people')
+}
+
+main()
